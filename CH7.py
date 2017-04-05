d6fdd60db22e837619f0231fda74010ee10a3c40
intenfas <abc5555666@yahoo.com.tw>
2017-04-05 21:01:28 +0800

    0.0

--- CH7.py
+++ CH7.py
@@ -2,193 +2,125 @@
 from __future__ import division
 import math, random
 
-#常態函數分布累積
-def normal_cdf(x, mu=0,sigma=1):
-    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
-
-#計算Z值
-def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
-    """find approximate inverse using binary search"""
-    # if not standard, compute standard and rescale
-    if mu != 0 or sigma != 1:
-        return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
-
-    low_z, low_p = -10.0, 0  # normal_cdf(-10) is (very close to) 0
-    hi_z, hi_p = 10.0, 1  # normal_cdf(10)  is (very close to) 1
-    while hi_z - low_z > tolerance:
-        mid_z = (low_z + hi_z) / 2  # consider the midpoint
-        mid_p = normal_cdf(mid_z)  # and the cdf's value there
-        if mid_p < p:
-            # midpoint is still too low, search above it
-            low_z, low_p = mid_z, mid_p
-        elif mid_p > p:
-            # midpoint is still too high, search below it
-            hi_z, hi_p = mid_z, mid_p
-        else:
-            break
-
-    return mid_z
-
-
-def normal_approximation_to_binomial(n, p):
-    """finds mu and sigma corresponding to a Binomial(n, p)"""
-    mu = p * n
-    sigma = math.sqrt(p * (1 - p) * n)
-    return mu, sigma
-
-
-#####
-#
-# probabilities a normal lies in an interval
-#
-######
-
-# the normal cdf _is_ the probability the variable is below a threshold
-normal_probability_below = normal_cdf
-
-
-# it's above the threshold if it's not below the threshold
-def normal_probability_above(lo, mu=0, sigma=1):
-    return 1 - normal_cdf(lo, mu, sigma)
-
-
-# it's between if it's less than hi, but not less than lo
-def normal_probability_between(lo, hi, mu=0, sigma=1):
-    return normal_cdf(hi, mu, sigma) - normal_cdf(lo, mu, sigma)
-
-
-# it's outside if it's not between
-def normal_probability_outside(lo, hi, mu=0, sigma=1):
-    return 1 - normal_probability_between(lo, hi, mu, sigma)
-
-
-######
-#
-#  normal bounds
-#
-######
-
-
-def normal_upper_bound(probability, mu=0, sigma=1):
-    """returns the z for which P(Z <= z) = probability"""
-    return inverse_normal_cdf(probability, mu, sigma)
-
-
-def normal_lower_bound(probability, mu=0, sigma=1):
-    """returns the z for which P(Z >= z) = probability"""
-    return inverse_normal_cdf(1 - probability, mu, sigma)
-
-
-def normal_two_sided_bounds(probability, mu=0, sigma=1):
-    """returns the symmetric (about the mean) bounds
-    that contain the specified probability"""
-    tail_probability = (1 - probability) / 2
-
-    # upper bound should have tail_probability above it
-    upper_bound = normal_lower_bound(tail_probability, mu, sigma)
-
-    # lower bound should have tail_probability below it
-    lower_bound = normal_upper_bound(tail_probability, mu, sigma)
-
-    return lower_bound, upper_bound
-
-
-def two_sided_p_value(x, mu=0, sigma=1):
-    if x >= mu:
-        # if x is greater than the mean, the tail is above x
-        return 2 * normal_probability_above(x, mu, sigma)
-    else:
-        # if x is less than the mean, the tail is below x
-        return 2 * normal_probability_below(x, mu, sigma)
-
-
-def count_extreme_values():
-    extreme_value_count = 0
-    for _ in range(100000):
-        num_heads = sum(1 if random.random() < 0.5 else 0  # count # of heads
-                        for _ in range(1000))  # in 1000 flips
-        if num_heads >= 530 or num_heads <= 470:  # and count how often
-            extreme_value_count += 1  # the # is 'extreme'
-
-    return extreme_value_count / 100000
-
-
-upper_p_value = normal_probability_above
-lower_p_value = normal_probability_below
-
-
-##
-#
-# P-hacking
-#
-##
-
-def run_experiment():
-    """flip a fair coin 1000 times, True = heads, False = tails"""
-    return [random.random() < 0.5 for _ in range(1000)]
-
-
-def reject_fairness(experiment):
-    """using the 5% significance levels"""
-    num_heads = len([flip for flip in experiment if flip])
-    return num_heads < 469 or num_heads > 531
-
-
-##
-#
-# running an A/B test
-#
-##
-
-def estimated_parameters(N, n):
-    p = n / N
-    sigma = math.sqrt(p * (1 - p) / N)
-    return p, sigma
-
-
-def a_b_test_statistic(N_A, n_A, N_B, n_B):
-    p_A, sigma_A = estimated_parameters(N_A, n_A)
-    p_B, sigma_B = estimated_parameters(N_B, n_B)
-    return (p_B - p_A) / math.sqrt(sigma_A ** 2 + sigma_B ** 2)
-
-
-##
-#
-# Bayesian Inference
-#
-##
-
-def B(alpha, beta):
-    """a normalizing constant so that the total probability is 1"""
-    return math.gamma(alpha) * math.gamma(beta) / math.gamma(alpha + beta)
-
-
-def beta_pdf(x, alpha, beta):
-    if x < 0 or x > 1:  # no weight outside of [0, 1]
-        return 0
-    return x ** (alpha - 1) * (1 - x) ** (beta - 1) / B(alpha, beta)
-
-z=a_b_test_statistic(1500,400,1400,350)
 
 if __name__ == "__main__":
+    # coding=utf-8
+    #from __future__ import division
+    #import math, random
     #第一題 假設檢定
     #設定mu = 98 , sigma = 10
     mu_0, sigma_0 = 98,10
     print "mu_0", mu_0
     print "sigma_0", sigma_0
+
+
+    # 常態函數分布累積method
+    def normal_cdf(x, mu=0, sigma=1):
+        return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
+    #逼近Z值method
+    def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
+        """find approximate inverse using binary search"""
+        # if not standard, compute standard and rescale
+        if mu != 0 or sigma != 1:
+            return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
+
+        low_z, low_p = -10.0, 0  # normal_cdf(-10) is (very close to) 0
+        hi_z, hi_p = 10.0, 1  # normal_cdf(10)  is (very close to) 1
+        while hi_z - low_z > tolerance:
+            mid_z = (low_z + hi_z) / 2  # consider the midpoint
+            mid_p = normal_cdf(mid_z)  # and the cdf's value there
+            if mid_p < p:
+                # midpoint is still too low, search above it
+                low_z, low_p = mid_z, mid_p
+            elif mid_p > p:
+                # midpoint is still too high, search below it
+                hi_z, hi_p = mid_z, mid_p
+            else:
+                break
+
+        return mid_z
+
+    #求Z值的method
+    def normal_lower_bound(probability, mu=0, sigma=1):
+        """returns the z for which P(Z >= z) = probability"""
+        return inverse_normal_cdf(1 - probability, mu, sigma)
+
+    #第一個問題
     # a = 5% = 0.95機率
     print "normal_lower_bound(0.95, mu_0, sigma_0)", normal_lower_bound(0.95, mu_0, sigma_0)
     print
+
     #第一題第二個問題
     print "normal_lower_bound(0.9, mu_0, sigma_0) = ", normal_lower_bound(0.9, mu_0, sigma_0)
 
-    #第一題第三個問題
-    #
-    print "normal_probability_below(x,mu,sigma) = ", normal_probability_below(81.55,98, 10)
+    #第一題第三個問題，求顯著性
+    #normal_probability_below = normal_cdf
+    print "normal_probability_belowx,mu,sigma) = ", normal_cdf(81.55,98, 10)
     print ("")
+
     #第二題 信賴區間
+
+    #右尾檢定
+    def normal_upper_bound(probability, mu=0, sigma=1):
+        """returns the z for which P(Z <= z) = probability"""
+        return inverse_normal_cdf(probability, mu, sigma)
+
+    #左尾檢定
+    def normal_lower_bound(probability, mu=0, sigma=1):
+        """returns the z for which P(Z >= z) = probability"""
+        return inverse_normal_cdf(1 - probability, mu, sigma)
+
+    #雙尾檢定
+    def normal_two_sided_bounds(probability, mu=0, sigma=1):
+        """returns the symmetric (about the mean) bounds
+        that contain the specified probability"""
+        tail_probability = (1 - probability) / 2
+
+        # upper bound should have tail_probability above it
+        upper_bound = normal_lower_bound(tail_probability, mu, sigma)
+
+        # lower bound should have tail_probability below it
+        lower_bound = normal_upper_bound(tail_probability, mu, sigma)
+
+        return lower_bound, upper_bound
+
     print "Confidence Intervals"
     print "normal_two_sided_bounds(信賴水準,平均數,標準差) = ",normal_two_sided_bounds(0.95,4.015,0.02)
 
+    #第三題 A/B Testing
+
+    #計算p(期望值/平均數) sigma(標準差)
+    def estimated_parameters(N, n):
+        p = n / N
+        sigma = math.sqrt(p * (1 - p) / N)
+        return p, sigma
+
+    #計算兩者差距
+    def a_b_test_statistic(N_A, n_A, N_B, n_B):
+        p_A, sigma_A = estimated_parameters(N_A, n_A)
+        p_B, sigma_B = estimated_parameters(N_B, n_B)
+        return (p_B - p_A) / math.sqrt(sigma_A ** 2 + sigma_B ** 2)
+
+    z = a_b_test_statistic(1500, 400, 1400, 350)
     print "PB-PA間的差距",(z)
+
+    #計算p-value值
+
+    normal_probability_below = normal_cdf
+
+
+    # it's above the threshold if it's not below the threshold
+    def normal_probability_above(lo, mu=0, sigma=1):
+        return 1 - normal_cdf(lo, mu, sigma)
+
+
+    def two_sided_p_value(x, mu=0, sigma=1):
+        if x >= mu:
+            # if x is greater than the mean, the tail is above x
+            return 2 * normal_probability_above(x, mu, sigma)
+        else:
+            # if x is less than the mean, the tail is below x
+            return 2 * normal_probability_below(x, mu, sigma)
+
     print "檢定兩者之間是否有差異",(two_sided_p_value(z))
